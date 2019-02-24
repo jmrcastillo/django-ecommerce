dfcc93d5143b157d2dd0898bd9eea9858ae5b9ef
jmrcastillo <jm.cass20@gmail.com>
2019-02-24 00:08:31 +0000

    modified cart app

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,7 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-        path('', views.cart_home, name='cart'),
+        path('', views.cart_detail, name='cart_detail'),
+        path('add/<int:product_id>/', views.cart_add, name='cart_add'),
+        path('remove/<int:product_id>/', views.cart_remove, name='cart_remove')
         ]
