7dcc842688853b0c86adaf89146653ac3dff1b64
jmrcastillo <jm.cass20@gmail.com>
2019-06-05 08:26:45 +0000

    modify celery - tasks.py

--- orders/tasks.py
+++ orders/tasks.py
@@ -14,7 +14,7 @@ def order_created(order_id):
     order = Order.objects.get(id=order_id)
     subject = 'Order nr. {}'.format(order_id)
     message = 'Dear {}, \n\n You have successfully placed an order.\
-            Your order id is {}.format(order.first_name, order_id)'
+            Your order id is {}'.format(order.first_name, order_id)
     mail_sent = send_mail(subject,
                           message,
                           'admin@myshop.com',
